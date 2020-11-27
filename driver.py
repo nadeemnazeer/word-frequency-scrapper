03e5e19bf1d57127716d3fa163a43335dc0d4173
Nadeem Nazeer <nazeer.nadeem@gmail.com>
2020-11-28 01:36:25 +0530

    Add Recursive scraping, with level and domain control via params

--- driver.py
+++ driver.py
@@ -5,15 +5,19 @@ import re
 
 
 def get_page_html(url):
-    payload = {}
-    headers = {
-    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    }
+    try:
+        print("Fetching..",url)
+        payload = {}
+        headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        }
 
-    response = requests.request("GET", url, headers=headers, data = payload)
+        response = requests.request("GET", url, headers=headers, data = payload)
 
-    return response.text
+        return response.text
+    except Exception as e:
+        return ""
 
 def clean_normalize_string(string):
     #find and replace alphanumeric chars with single spaces
@@ -42,31 +46,57 @@ def get_visible_tag(element):
         return False
     return True
 
-
-def get_freq_words(url, ngram=1):
+def get_internal_links(soup, scrap_domains):
+    link_list = set()
+    for link in soup.find_all('a', href=True):
+        href = link['href']
+        for domain in scrap_domains:
+            if domain in href:
+                link_list.add(link['href'])
+    return link_list
+    
+url_visited = []
+def get_sentence_ngrams_list(url,scrap_domains, ngram=1,max_level=4, level=1):
+    url_visited.append(url)
+    ngramlist = []
+    #TODO: Can be improved by checking if url already fetched
+    
     body = get_page_html(url)
     body = body.replace("<br />","")
     soup = BeautifulSoup(body, 'html.parser')
+    #print(get_internal_links(soup))
+
     texts = soup.findAll(text=True)
     texts_visible_tags = filter(get_visible_tag, texts)
-    #filter(texts_visible_tags)
     sentences = []
     for text in  filter(None, texts_visible_tags):
         sub_sentences = text.strip().lower().split(".")
         sentences.extend(sub_sentences)
 
-    wordlist = []
+    
     for sentence in sentences:
-        wordlist.extend(compose_ngrams(sentence, ngram))
-    wordfreq = [wordlist.count(w) for w in wordlist]
-    lt = set(zip(wordlist, wordfreq))
-    sorted_by_freq = sorted(lt, key=lambda tup:tup[1], reverse=True)
-    print(sorted_by_freq[:15])
+        ngramlist.extend(compose_ngrams(sentence, ngram))
+    for link in get_internal_links(soup,scrap_domains):
+        if level > max_level:
+            return ngramlist
+        else:
+            level += 1
+            if link not in url_visited:
+                ngramlist.extend(get_sentence_ngrams_list(link,scrap_domains, ngram, level, max_level))
+    return ngramlist
 
 
+
+def get_freq_words(url, scrap_domains, ngram=1,top=10, max_level=4):
+    sentence_ngrams_list = get_sentence_ngrams_list(url,scrap_domains, ngram,max_level)
+    ngramfreq = [sentence_ngrams_list.count(ngram) for ngram in sentence_ngrams_list]
+    unique_ngram_freq = set(zip(sentence_ngrams_list, ngramfreq))
+    sorted_by_freq = sorted(unique_ngram_freq, key=lambda tup:tup[1], reverse=True)
+    print(sorted_by_freq[:top])
+    
     
 
     
-get_freq_words('https://www.314e.com/',3)
+get_freq_words('https://www.nytimes.com/',["nytimes.com"], ngram=2,top=10, max_level=4)
 #Test data to check against view:source via browser
 #[('know more', 6), ('healthcare it', 5), ('cures act', 5), ('ehr help', 4), ('go live', 4)
