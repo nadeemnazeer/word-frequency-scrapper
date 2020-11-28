ad0a254343d7d25f3b0cf1fb2681fe0c43608f7e
Nadeem Nazeer <nazeer.nadeem@gmail.com>
2020-11-28 17:08:05 +0530

    Add command line argument cabability to grpc_client as well

--- grpc_client.py
+++ grpc_client.py
@@ -9,26 +9,35 @@ import grpc
 
 import grpc_stubs.freq_scrap_pb2 as freq_scrap_pb2
 import grpc_stubs.freq_scrap_pb2_grpc as freq_scrap_pb2_grpc
+import argparse
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tool to scrap any website and fetch top ngrams with their frequency. e.g: python grpc_client.py --url="https://www.washingtonpost.com/" --scrap_domains="washingtonpost.com"  --ngram=1 --top=10 --max_level=4')
 
+    parser.add_argument('--url',type=str, help='Base url to scrap.', required=True)
+    parser.add_argument('--scrap_domains',type=str,help='Comma seperated list of domains to restrict scraping and cralwing to.', required=True)
+    parser.add_argument('--ngrams',type=int, help='ngrams value', required=True)
+    parser.add_argument('--top',type=int, help='Top limit', required=True)
+    parser.add_argument('--max_level',type=int, help='Max level to cralw & scrap', required=True)
 
-def run():
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
+    arguments = parser.parse_args()
+    url = arguments.url
+    scrap_domains = arguments.scrap_domains
+    ngrams = arguments.ngrams
+    top = arguments.top
+    max_level = arguments.max_level
+    
     with grpc.insecure_channel('localhost:50051') as channel:
-        url = 'https://www.washingtonpost.com/'
-        scrap_domains = ','.join(["washingtonpost.com"])
-        ngrams = 2
-        top = 10
-        max_level = 1
+        url = url
+        scrap_domains = scrap_domains
+        ngrams =ngrams
+        top = top
+        max_level =max_level
        
         request = freq_scrap_pb2.Request(url=url,scrap_domains=scrap_domains,ngrams= ngrams,top=top,max_level=max_level  )
         stub = freq_scrap_pb2_grpc.FreqScrapStub(channel)
 
         response = stub.GetFreq(request)
         print(response)
-
-
-
-if __name__ == '__main__':
-    logging.basicConfig()
-    run()
